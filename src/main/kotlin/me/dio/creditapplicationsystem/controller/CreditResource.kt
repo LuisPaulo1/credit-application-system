65da2136774cdadf081d76f1c5691f29dbc917a0
LuisPaulo1 <csluispaulo@gmail.com>
2024-01-28 19:24:31 -0300

    Endpoints CreditResource-(findAllCustomerCredits)

--- src/main/kotlin/me/dio/creditapplicationsystem/controller/CreditResource.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/controller/CreditResource.kt
@@ -1,12 +1,15 @@
 package me.dio.creditapplicationsystem.controller
 
 import me.dio.creditapplicationsystem.dto.CreditDto
+import me.dio.creditapplicationsystem.dto.CreditViewList
 import me.dio.creditapplicationsystem.entity.Credit
 import me.dio.creditapplicationsystem.service.impl.CreditService
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/api/credits")
@@ -16,5 +19,11 @@ class CreditResource(private val creditService: CreditService) {
   fun saveCredit(@RequestBody creditDto: CreditDto): String {
     val credit: Credit = creditService.save(creditDto.toEntity())
     return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+  }
+
+  @GetMapping
+  fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditViewList> {
+    val credits: List<Credit> = creditService.findAllByCustomer(customerId)
+    return credits.map { CreditViewList(it) }
   }
 }
